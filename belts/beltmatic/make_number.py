547f6578494b976991f785d6aa68c72af1d2326e
19690ao <19690ao@gmail.com>
2024-06-26 16:43:59 -0400

    Meet In The Middle

--- belts/beltmatic/make_number.py
+++ belts/beltmatic/make_number.py
@@ -31,6 +31,8 @@ def invert(symbol, b, c):
     assert b==b//1 and c==c//1
     match symbol:
         case '+':
+            if c<b:
+                return c
             return c - b
         case '*':
             # If a*b=c, a=c//b IFF b!=0 and c%b=0
@@ -63,54 +65,145 @@ def minimal_solution(target, using):
     if target in using:
         return str(target)
     operator_symbols = {'+', '*', '-', '/', '^'}
-
-    def heuristic(number):
-        return abs(target - number)
     
     counter = 1
     path_score = functools.cmp_to_key(path_cmp)
     def queue_input(path, path_length, counter=0):
         number = path[-1][0]
-        return ((path_length, heuristic(number), path_score(path), counter), path)
+        return ((path_length, abs(target - number), path_score(path), counter), path)
 
-    
+    def queue_input_back(path, path_length, counter=0):
+        # TODO: Put a heuristic in here between counter and path_length
+        # min_using = min(using)
+        # max_using =
+        # abs(target - number)
+        return ((path_length, counter), path)
+
     queue = [queue_input([(number, (None, number))], 1) for number in using]
     heapq.heapify(queue)
+
+    queue_back = [queue_input_back([(target, None)], 0)]
+    heapq.heapify(queue_back)
     # print(queue)
 
     visited = dict([(number, 1) for number in using])
-    while queue:
-        score, path = heapq.heappop(queue)
-        path_length, _, _, _ = score
-        node, _ = path[-1]
-        if node != target and target in visited and visited[target] < path_length:
-            break
-        if node in visited and visited[node] < path_length:
-            break
-        if node == target:
-            # print(parsed_solution(path, operator_dict))
-            return parsed_solution(path)
-        else:
-            edges = [(operator, operand) for operator in operator_symbols for operand in using]
-            neighbours = set()
-            for edge in edges:
-                operator, operand = edge
-                neighbour = operate(operator, node, operand)
-                if neighbour in {node, operand}:
-                    continue
-                new_path_length = path_length+1
-                if neighbour in visited and visited[neighbour] < new_path_length:
-                    continue
-                new_path = path.copy()
-                new_path.append((neighbour, edge))
-                new_input = queue_input(new_path, new_path_length, counter)
-                if neighbour in visited and visited[neighbour] < new_path_length:
-                    continue
+    visited_back = dict([(target, 0)])    
+
+    start_paths = []
+    end_dict = {target: [[(target, None)]]}
+
+    end_paths = []
+    start_dict = dict([(number,[[number, (None, number)]]) for number in using])
+
+    # while not done
+    while queue or queue_back:
+        # Get area around the start until next length
+        new_queue = []
+        heapq.heapify(new_queue)
+        while queue:
+            score, path = heapq.heappop(queue)
+            path_length, _, _, _ = score
+            node, _ = path[-1]
+            if node in visited_back:
+                start_paths.append(path)
+            else:
+                edges = [(operator, operand) for operator in operator_symbols for operand in using]
                 
-                heapq.heappush(queue, new_input)
-                visited[neighbour] = new_path_length
-                counter += 1
-                neighbours.add(neighbour)
+                for edge in edges:
+                    operator, operand = edge
+                    neighbor = operate(operator, node, operand)
+                    if neighbor in {node, operand}:
+                        continue
+                    new_path_length = path_length+1
+                    if neighbor in visited and visited[neighbor] < new_path_length:
+                        continue
+                    new_path = path.copy()
+                    new_path.append((neighbor, edge))
+                    new_input = queue_input(new_path, new_path_length, counter)
+                    
+                    if neighbor not in start_dict:
+                        start_dict[neighbor] = []
+                    start_dict[neighbor].append(new_path)
+                    heapq.heappush(new_queue, new_input)
+                    visited[neighbor] = new_path_length
+                    counter += 1
+        queue = new_queue
+        # If found, stop, mark
+        if start_paths != []:
+            # Do the return here
+            found_path = None
+            best_score = None
+            for start_path in start_paths:
+                middle = start_path[-1][0]
+                assert middle in end_dict
+                # print(middle)
+                # print(end_dict[middle])
+                for reversed_end_path in end_dict[middle]:
+                    end_path = list(reversed(reversed_end_path))
+                    total_path = start_path+end_path[1:]
+                    current_score = path_score(total_path)
+                    if best_score is None or best_score > current_score:
+                        found_path = total_path
+                        best_score = current_score
+            # print("Bing")
+            return parsed_solution(found_path)
+        # end_dict = dict()
+        # Get area around the end until next length
+        new_queue_back = []
+        heapq.heapify(new_queue_back)
+        while queue_back:
+            score, path = heapq.heappop(queue_back)
+            path_length, _ = score
+            node, _ = path[-1]
+            if node in visited:
+                end_paths.append(path)
+            else:
+                edges = [(operator, operand) for operator in operator_symbols for operand in using]
+                
+                for edge in edges:
+                    operator, operand = edge
+                    neighbor = invert(operator, operand, node)
+                    if neighbor in {node, operand}:
+                        continue
+                    new_path_length = path_length+1
+                    if neighbor in visited_back and visited_back[neighbor] < new_path_length:
+                        continue
+                    new_path = path.copy()
+                    # This is gonna be weird
+                    new_path[-1] = (node, edge)
+                    new_path.append((neighbor, None))
+                    new_input = queue_input_back(new_path, new_path_length, counter)
+                    
+                    if neighbor not in end_dict:
+                        end_dict[neighbor] = []
+                    end_dict[neighbor].append(new_path)
+                    heapq.heappush(new_queue_back, new_input)
+                    visited_back[neighbor] = new_path_length
+                    counter += 1
+        queue_back = new_queue_back
+        # If found, stop, mark
+        if end_paths != []:
+            # Do the return here
+            found_path = None
+            best_score = None
+            # print(start_dict)
+            for reversed_end_path in end_paths:
+                end_path = list(reversed(reversed_end_path))
+                # print(end_path)
+                middle = end_path[0][0]
+                # print(f"Middle: {middle}")
+                assert middle in start_dict
+                for start_path in start_dict[middle]:
+                    total_path = start_path+end_path[1:]
+                    # print(parsed_solution(total_path))
+                    current_score = path_score(total_path)
+                    if best_score is None or best_score > current_score:
+                        found_path = total_path
+                        best_score = current_score
+            # print(best_score)
+            # print("Bong")
+            return parsed_solution(found_path)
+        # start_dict = dict()
     return None
 
 def numbers_in_path_set(path):
@@ -121,8 +214,9 @@ def numbers_in_path_list(path):
 
 def path_cmp(a, b):
     # Returns -1 if a<b, 0 if a=b, 1 if a>b
+    # print(f"Comparing {parsed_solution(a)} vs {parsed_solution(b)}")
     a_numbers, b_numbers = numbers_in_path_set(a), numbers_in_path_set(b)
-    a_unique, b_unique = len(a_numbers), len(a_numbers)
+    a_unique, b_unique = len(a_numbers), len(b_numbers)
     if a_unique != b_unique:
         return int(a_unique > b_unique)*2-1
     
@@ -139,7 +233,7 @@ def path_cmp(a, b):
     return 0
 
 def parsed_solution(raw_solution):
-    if raw_solution == None:
+    if raw_solution is None:
         return ''
     ans = str(raw_solution[0][0])
     if len(raw_solution) <= 1:
@@ -159,7 +253,64 @@ def test_div(allowed_numbers):
         if '/' in solution:
             print(f"{i}={solution}")
 
-if __name__ == "__main__":
+def score_tests():
+    path_score = functools.cmp_to_key(path_cmp)
+
+    path_a = [(1, (None, 1))]
+    path_b = [(1, (None, 1)), (1, ('*', 1))]
+    assert path_score(path_a) < path_score(path_b)
+
+    path_a = [(2, (None, 2)), (6, ("*", 3))]
+    path_b = [(2, (None, 1)), (6, ("+", 4))]
+    assert path_score(path_a) < path_score(path_b)
+
+    path_a = [(5, (None, 5)), (25, ('*', 5))]
+    path_b = [(17, (None, 17)), (25, ('+', 8))]
+    assert path_score(path_a) < path_score(path_b)
+
+    path_a = [(9, (None, 9)), (18, ('+', 9))]
+    path_b = [(3, (None, 3)), (18, ('*', 6))]
+    assert path_score(path_a) < path_score(path_b)
+
+def test_1():
+    allowed_numbers = [1]
+    number = 4
+    result = minimal_solution(number, allowed_numbers)
+    assert result == "((1+1)+1)+1"
+    number = 5
+    result = minimal_solution(number, allowed_numbers)
+    assert result == "(((1+1)+1)+1)+1"
+
+    allowed_numbers = [1, 2]
+    number = 4
+    result = minimal_solution(number, allowed_numbers)
+    assert result in {"2+2", "2*2", "2^2"}
+
+    allowed_numbers = [1, 2, 3]
+    number = 27
+    result = minimal_solution(number, allowed_numbers)
+    assert result == "3^3"
+
+def test_2():
+    allowed_numbers = [3, 6, 9]
+    number = 18
+    result = minimal_solution(number, allowed_numbers)
+    assert result == "9+9"
+
+def test_3():
+    allowed_numbers = [5, 8, 17]
+    number = 25
+    result = minimal_solution(number, allowed_numbers)
+    assert result == "5*5"
+
+def run_tests():
+    score_tests()
+    test_1()
+    test_2()
+    test_3()
+
+def main():
+    # run_tests()
     user_input = ""
     while not user_input.isdigit():
         user_input = input("Please enter an integer >> ").strip()
@@ -167,9 +318,13 @@ if __name__ == "__main__":
     print(f"How to make {number} in minimal numbers")
     allowed_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17]
     # allowed_numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+    # allowed_numbers = [1, 2]
     print(f"Allowed to use {allowed_numbers}")
     # test_div(allowed_numbers)
     solution = minimal_solution(number, allowed_numbers)
     if solution != None:
         print(f"Solution found")
         print(solution)
+
+if __name__ == "__main__":
+    main()
